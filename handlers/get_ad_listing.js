90be5a1484022ca5c791b0a61057231108dbbe96
Ky Quoc Viet <vietky@Kys-MacBook-Pro.local>
2019-11-01 18:42:49 +0700

    fix properties

--- handlers/get_ad_listing.js
+++ handlers/get_ad_listing.js
@@ -4,10 +4,9 @@ function getAds(req, res) {
     {
       title: 'abc 1',
       voice_title_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
-      subject: 'subject ne',
-      voice_subject_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
+      description: 'description ne',
+      voice_description_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
       images: [
-
       ],
       category: 5010,
       created_date: new Date(),
@@ -16,8 +15,20 @@ function getAds(req, res) {
     {
       title: 'abc 2',
       voice_title_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
-      subject: 'subject ne',
-      voice_subject_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
+      description: 'description ne',
+      voice_description_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
+      images: [
+        'https://storage.cloud.google.com/sugar-maroon/images/de7b96a0-fc77-11e9-bc49-730fdec8461d?folder=true&organizationId=true',
+      ],
+      category: 5010,
+      created_date: new Date(),
+      updated_date: new Date()
+    },
+    {
+      title: 'abc 4',
+      voice_title_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
+      description: 'description ne',
+      voice_description_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
       images: [
 
       ],
@@ -28,22 +39,10 @@ function getAds(req, res) {
     {
       title: 'abc 4',
       voice_title_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
-      subject: 'subject ne',
-      voice_subject_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
+      description: 'description ne',
+      voice_description_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
       images: [
-
-      ],
-      category: 5010,
-      created_date: new Date(),
-      updated_date: new Date()
-    },
-    {
-      title: 'abc 4',
-      voice_title_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
-      subject: 'subject ne',
-      voice_subject_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
-      images: [
-
+        'https://storage.cloud.google.com/sugar-maroon/images/de7b96a0-fc77-11e9-bc49-730fdec8461d?folder=true&organizationId=true'x
       ],
       category: 5010,
       created_date: new Date(),
@@ -52,10 +51,10 @@ function getAds(req, res) {
     {
       title: 'abc 5',
       voice_title_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
-      subject: 'subject ne',
-      voice_subject_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
+      description: 'description ne',
+      voice_description_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
       images: [
-
+        'https://storage.cloud.google.com/sugar-maroon/images/de7b96a0-fc77-11e9-bc49-730fdec8461d?folder=true&organizationId=true'
       ],
       category: 5010,
       created_date: new Date(),
