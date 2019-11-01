d1db48a0b1932b9333e2bab474d8e8036ac9343e
Ky Quoc Viet <vietky@Kys-MacBook-Pro.local>
2019-11-01 23:59:58 +0700

    fix

--- handlers/get_ad_listing.js
+++ handlers/get_ad_listing.js
@@ -42,7 +42,7 @@ function getAds(req, res) {
       description: 'description ne',
       voice_description_url: "https://storage.googleapis.com/sugar-maroon/records/402c8360-fc90-11e9-a051-2ff539329425",
       images: [
-        'https://storage.cloud.google.com/sugar-maroon/images/de7b96a0-fc77-11e9-bc49-730fdec8461d?folder=true&organizationId=true'x
+        'https://storage.cloud.google.com/sugar-maroon/images/de7b96a0-fc77-11e9-bc49-730fdec8461d?folder=true&organizationId=true'
       ],
       category: 5010,
       created_date: new Date(),
